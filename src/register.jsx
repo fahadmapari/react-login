e0d34eede8d36069e017500088e6315c458b227f
Fahad Mapari <btechy4@gmail.com>
2022-08-05 11:35:19 +0530

    initial setup

---
+++ src/register.jsx
@@ -0,0 +1,16 @@
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}^/;
+const PWD_REGEX = /^^/;
+
+const register = () => {
+  return <div>register</div>;
+};
+
+export default register;
