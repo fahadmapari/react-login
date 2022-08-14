7383ad5a1a029739a9068ca3f9bf4dcc0841241e
Fahad Mapari <btechy4@gmail.com>
2022-08-14 18:48:04 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,33 @@ import Layout from "./components/Layout";
 import Lounge from "./components/Lounge";
 import Missing from "./components/Missing";
 import Unathorized from "./components/Unathorized";
-import LinkPage from "./components/LinkPage";  
-
+import LinkPage from "./components/LinkPage";
 
 function App() {
   return (
     <main className="App">
-      {/* <Register /> */}
-      <Login />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="linkpage" element={<LinkPage />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+
+          {/* we want to protect these routes */}
+
+          <Route path="/" element={<Home />} />
+
+          <Route path="editor" element={<Editor />} />
+
+          <Route path="admin" element={<Admin />} />
+
+          <Route path="lounge" element={<Lounge />} />
+
+          {/* catch all */}
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
     </main>
   );
 }
