f653b715fe3d0c431594b9e442d5b9384624f75e
macos <macos@macoss-MacBook-Air.local>
2022-08-14 13:52:31 +0530

    idk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Register from "./Register";
 import Login from "./Login";
+import Admin from "./components/Admin";
+import Editor from "./components/Editor";
+import Layout from "./components/Layout";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
+import Unathorized from "./components/Unathorized";
+import LinkPage from "./components/LinkPage";  
+
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       {/* <Register /> */}
       <Login />
-    </div>
+    </main>
   );
 }
 
